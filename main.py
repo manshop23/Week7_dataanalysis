68e9d713e62577086865c3c7ac7fbf2129c41076
manshop23 <mr.ronnakon.pl.61@ubu.ac.th>
2021-12-14 10:12:46 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+class User(BaseModel):
+    username:str
+    password:str
+    level: Optional[str] = "nomal"
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+#http://127.0.0.1:8000/hi?name=manny&reply=1234
+@app.get("/hi")
+def hi(name:str, reply:str  ): 
+    return {"hi": name,"reply": reply}
+
+#http://127.0.0.1:8000/items/1
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/login")
+def login(user : User):
+    return {"echo": user}
